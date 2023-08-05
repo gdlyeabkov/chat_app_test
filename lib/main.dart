e80187b97eee8a6deadf006a24e73187ebddbceb
softtrack <glebdyakov2000@gmail.com>
2023-08-05 08:57:36 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chat_app_test/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat app test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
