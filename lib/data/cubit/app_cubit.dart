f1de0a291c74731dfc201e5e32d5b6fbdb41968e
softtrack <glebdyakov2000@gmail.com>
2023-08-07 17:14:53 +0300

    add init cubit

--- lib/data/cubit/app_cubit.dart
+++ lib/data/cubit/app_cubit.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:chat_app_test/data/constants/hosts.dart';
 import 'package:chat_app_test/data/models/chat.dart';
+import 'package:chat_app_test/data/utils/mocks.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:universal_html/html.dart';
 
 part 'app_state.dart';
 
@@ -22,4 +27,28 @@ class AppCubit extends Cubit<AppState> {
       timer: null,
     ),
   );
+
+  init() async {
+    emit(state.copyWith(
+      chats: Mocks.chats,
+    ));
+    final dio = Dio();
+    final response = await dio.get('${Hosts.API_DOMAIN}/posts');
+    final status = response.statusCode;
+    final isSuccessfull = status == 200;
+    if (isSuccessfull) {
+      String? data = window.localStorage['data'];
+      if (data != null) {
+        Map chatList = jsonDecode(data);
+        Iterable rawChats = chatList['chats'];
+        var val = rawChats.map<Chat>((e) {
+          var someChat = Chat.fromJson(e);
+          return someChat;
+        }).toList();
+        emit(state.copyWith(
+          chats: val,
+        ));
+      }
+    }
+  }
 }
