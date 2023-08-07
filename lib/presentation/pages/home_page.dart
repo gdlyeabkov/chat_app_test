6273de72bd7981bac4fcfd2d6ea11c2bdc82c5ef
softtrack <glebdyakov2000@gmail.com>
2023-08-07 17:35:42 +0300

    connect widgets to home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:chat_app_test/data/cubit/app_cubit.dart';
+import 'package:chat_app_test/presentation/widgets/chat_detail_widget.dart';
+import 'package:chat_app_test/presentation/widgets/chats_aside_widget.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required AppCubit appCubit});
+  const MyHomePage({
+    required this.appCubit,
+    super.key,
+  });
+
+  final AppCubit appCubit;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -12,8 +19,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: null,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Row(
+            children: [
+              ChatsAsideWidget(
+                appCubit: widget.appCubit,
+              ),
+              widget.appCubit.state.selectedChat.id > -1
+                  ? ChatDetailWidget(
+                appCubit: widget.appCubit,
+              )
+                  : Container(
+                width: MediaQuery.of(context).size.width -
+                    (MediaQuery.of(context).size.width / 100 * 34.6),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Выберите чат,\nчтобы начать общаться',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
